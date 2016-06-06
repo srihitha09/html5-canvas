344398eb9e81e36b71b8295360dd2b8ad7f143f8
Srihitha <srihitha.maryada@gmail.com>
2016-06-05 23:08:25 -0400

    Added tranisition pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,88 @@
-window.onload = function() {
+window.onload = loadStartPage();
+
+//Loads the start page and hides canvas
+function loadStartPage(){
+    document.getElementById("gamePage").style.display= "none";
+    document.getElementById("transButton").onclick = function() {loadCanvasLevel()};
+} 
+
+var countdownTimer;
+var seconds;
+//timer for each level
+function timer(){
+    var remainingSeconds = seconds;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;  
+    }
+    document.getElementById('timer').innerHTML = remainingSeconds + " seconds";
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        loadTransitional();
+    } else {
+        seconds--;
+    }
+}
+
+function updateTimer(){
+    seconds = 60;
+    countdownTimer = setInterval('timer()', 1000);
+}
+
+function pauseTimer(){
+    debugger;
+    var text = document.getElementById("pause").innerHTML;
+    if(text === "Pause"){
+        document.getElementById("pause").innerHTML = "Resume";
+        clearInterval(countdownTimer);
+    }
+    else{
+        document.getElementById("pause").innerHTML = "Pause";
+        countdownTimer = setInterval('timer()', 1000);
+    }
     
+}
+
+//loads transitional page
+function loadTransitional(){
+    var level = "1";
+    loadStartPage();
+    document.getElementById("startPage").style.display= "block";
+    document.getElementById("title").innerHTML="Level # " + level + "";
+    document.getElementById("transButton").innerHTML = "Next";
+    document.getElementById("transButton").onclick = function() {loadCanvasLevel()};
+}
+
+function loadCanvasLevel(){
+    
+    //Logic to check level and load appropriately
+    //if level == 1
+    loadCanvas();
+}
+
+function loadFinishPage(){
+
+}
+
+
+//Loads the canvas and hides start page
+function loadCanvas() { 
+    updateTimer();
+    document.getElementById("startPage").style.display= "none";
+    document.getElementById("gamePage").style.display= "block";
     var c = document.getElementById("main");
+    c.width = '1000';
+    c.height = '640';
     window.ctx = c.getContext("2d"); // Dealing with a global context is easier
     //addShapes();
-    animate();
-    
-} // window.onload
+    animate();  
+}
 
+
+/* 
+* Objects
+*/
+
+//Moon
 var moon = {
 
     x:95,
@@ -23,14 +99,12 @@ var moon = {
         ctx.beginPath();
         ctx.arc(this.x+7, this.y, 20, 1.68*Math.PI, 0.32*Math.PI, true);
         ctx.stroke();
-        ctx.closePath();
-        
-
-        
+        ctx.closePath();       
     }
 
 };
 
+//Star
 var star = {
 	x:50,
 	y:50,
@@ -43,9 +117,7 @@ var star = {
 
 	draw: function(){
 		ctx.beginPath();
-		    ctx.fillStyle = "blue";
-    
-    	
+		    ctx.fillStyle = "blue";    	
         for (var ixVertex = 0; ixVertex <= 2 * 6; ++ixVertex) {
             var angle = ixVertex * Math.PI / 6 - Math.PI / 2;
             var radius = ixVertex % 2 == 0 ? 25 : 10;
@@ -57,12 +129,12 @@ var star = {
 
 function addShapes(){
 
-
 }
 
+//Animation
 function animate(){
-        var shapes = new Array(star,moon);
-	    var width = 1000;
+    var shapes = new Array(star,moon);
+    var width = 1000;
     var height = 640;
 	window.ctx.clearRect(0,0,width,height);
 
